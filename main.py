776112f1d6fc89522868b072863780f138466b72
55AAh <kostya.kulik@gmail.com>
2021-07-17 16:20:05 +0300

    Trying to send normal response

--- main.py
+++ main.py
@@ -16,12 +16,9 @@ def main():
         data = client_sock.recv(10240)
         #data = data.decode("utf-8")
         print(f"\tRECEIVED:\n{data}")
-        from time import sleep
-        sleep(1)
-        response = "HTTP/1.0 200 OK\r\nContent-type: text/html\r\n\r\n"
+        response = "HTTP/1.0 200 OK\r\nServer: BaseHTTP/0.6 Python/3.9.5\r\nDate: Sat, 17 Jul 2021 13:18:48 GMT\r\nContent-type: text/html\r\n\r\nPOST request for /"
         response = response.encode("utf-8")
         client_sock.send(response)
-        sleep(1)
         client_sock.close()
         print(f"\tCLIENT SERVED")
 
