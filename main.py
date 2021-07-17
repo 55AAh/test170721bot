d2d58bd6bf5f00321e7e8a31a9fa4bc29c890d43
55AAh <kostya.kulik@gmail.com>
2021-07-17 14:55:18 +0300

    Basic sockets

--- main.py
+++ main.py
@@ -1,5 +1,18 @@
+import socket
+
+
 def main():
-    pass
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.bind(("127.0.0.1", 80))
+    print("LISTENING")
+    sock.listen(10)
+    while True:
+        (client_sock, address) = sock.accept()
+        print(f"CONNECTION: {address}")
+        data = client_sock.recv(10240)
+        data = data.decode("utf-8")
+        print(f"RECEIVED:\n{data}")
+        print(f"CLIENT SERVED")
 
 
 if __name__ == '__main__':
