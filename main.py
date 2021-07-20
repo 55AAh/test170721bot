f7b66b1c0624f947022978f9716dd079367fc98d
55AAh <kostya.kulik@gmail.com>
2021-07-20 10:25:12 +0300

    TEST: self-notify and SIGTERM handling

--- main.py
+++ main.py
@@ -1,19 +1,53 @@
-from logging import log, INFO
+from threading import Thread
+from time import sleep
+import requests
 import os
+from logging import log, ERROR
+from http.server import HTTPServer, BaseHTTPRequestHandler
+import signal
 
-from logger import Logger
-from server import Server
+h=[None]
+s=[False]
 
+def t():
+    def run(server_class=HTTPServer, handler_class=BaseHTTPRequestHandler):
+        server_address = ('0.0.0.0', int(os.getenv("PORT", 80)))
+        httpd = server_class(server_address, handler_class)
+        h[0]=httpd
+        httpd.serve_forever()
+    run()
 
 def main():
     Logger("log.txt")
-    log(INFO, "\tSTARTING...")
-
-    HOST = "0.0.0.0"
-    PORT = int(os.getenv("PORT", 80))
-    server = Server(HOST, PORT)
-    server.start()
-
+    log(ERROR, "\tSTARTING")
+    th=Thread(target=t,args=())
+    th.start()
+    def sss(signum, frame):
+        print(signum, frame)
+        h[0].shutdown()
+        s[0]=True
+    signal.signal(signal.SIGTERM, sss)
+    log(ERROR, "\tSTART")
+    sleep(20 * 60)
+    log(ERROR, "\tNTF1")
+    requests.get("https://test170721.herokuapp.com/notify")
+    sleep(20 * 60)
+    log(ERROR, "\tNTF2")
+    requests.get("https://test170721.herokuapp.com/notify")
+    i=0
+    while not s[0]:
+        if i % 1 == 0:
+            log(ERROR, i)
+        i+=1
+        sleep(1)
+    th.join()
+    log(ERROR, "\tSAVING DATA")
+    i=0
+    while i < 60*60:
+        log(ERROR, i)
+        i += 1
+        sleep(1)
+    return
 
 if __name__ == '__main__':
     main()
