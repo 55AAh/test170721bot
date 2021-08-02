28c630f2aff76af337e259e0774ca9aa00ff9447
55AAh <kostya.kulik@gmail.com>
2021-08-02 14:47:10 +0300

    Added logging lock & various fixes

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import multiprocessing
-from multiprocessing import Process, Event
+from multiprocessing import Process, Event, Lock
 from threading import Thread
 from signal import signal, SIGTERM
 from time import sleep
@@ -8,34 +8,43 @@ import socket
 from http.server import HTTPServer, SimpleHTTPRequestHandler
 
 
+ll = [0]
+def log(*args, **kwargs):
+	with ll[0]:
+		print(*args, **kwargs)
+
+
 def reg_signal(name, event):
 	def callback(*args, **kwargs):
-		print(f"Caught SIGTERM in {name}: {args}, {kwargs}")
+		log(f"Caught SIGTERM in {name}: {args}, {kwargs}")
 		if event:
 			event.set()
 	signal(SIGTERM, callback)
 	
 
-def counter(name, event):
+def counter(_ll, name, event):
+	print("LOOOOOOOOOOCK", _ll)
+	ll[0] = _ll
 	reg_signal(name, event)
 	i = 0
 	while True:
 		sleep(1)
 		i += 1
-		print(name, ">", i)
+		log(name, ">", i)
 
 
 def stopper(event, httpd):
 	event.wait()
-	print("GOT EVENT")
+	log("GOT EVENT")
 	httpd.shutdown()
 
 
-def main():
+def main(_ll):
+	ll[0] = _ll
 	event=Event()
 	processes = []
-	for i in range(3):
-		p = Process(target=counter, args=("Process " + str(i + 1), None), daemon=True)
+	for i in range(1):
+		p = Process(target=counter, args=(ll[0], "Process " + str(i + 1), None), daemon=True)
 		processes.append(p)
 		p.start()
 	reg_signal("main", event)
@@ -44,15 +53,16 @@ def main():
 	httpd = HTTPServer((HOST, PORT), SimpleHTTPRequestHandler)
 	Thread(target=stopper, args=(event, httpd)).start()
 	httpd.serve_forever()
-	print("EXITING")
+	log("EXITING")
 	for p in processes:
 		p.kill()
-	print("EXITED")
+	log("EXITED")
 	
 
 if __name__ == '__main__':
 	multiprocessing.set_start_method("spawn")
-	print(f"START METHOD = {multiprocessing.get_start_method()}")
-	Process(target=main).start()
+	ll[0]=Lock()
+	log(f"START METHOD = {multiprocessing.get_start_method()}")
+	Process(target=main, args=(ll[0],)).start()
 	sleep(90)
-	print("ME")
+	log("ME")
