65d6ff57678b2721fbc9b2cbfdf696e0b92ccfb9
55AAh <kostya.kulik@gmail.com>
2021-08-02 12:40:33 +0300

    Testing SIGTERM in Heroku processes (my god please)

--- main.py
+++ main.py
@@ -11,20 +11,25 @@ def rs(n):
 	signal(SIGTERM, sc)
 	
 
-def main():
-	for i in range(5):
-		Process(target=pt, args=("Process " + str(i + 1),)).start()
-	rs("Main")
+def ctr():
 	i = 0
-	HOST = "0.0.0.0"
-	PORT = int(os.getenv("PORT", 80))
-	sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-	sock.bind((HOST, PORT))
-	print(f"BOUND TO {HOST}:{PORT}")
 	while True:
 		sleep(1)
 		i += 1
 		print(i)
+	
+	
+def main():
+	for i in range(5):
+		Process(target=pt, args=("Process " + str(i + 1),)).start()
+	rs("Main")
+	HOST = "0.0.0.0"
+	PORT = int(os.getenv("PORT", 80))
+	sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	sock.bind((HOST, PORT))
+	Process(target=ctr).start()
+	sock.listen()
+	print(f"LISTENING ON {HOST}:{PORT}")
 
 
 def pt(i):
