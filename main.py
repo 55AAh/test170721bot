03137d8ded40355833343df4c16c7610e588401a
55AAh <kostya.kulik@gmail.com>
2021-08-02 15:16:41 +0300

    Injected custom serve_forever in httpd

--- main.py
+++ main.py
@@ -7,11 +7,15 @@ import os
 import socket
 from http.server import HTTPServer, SimpleHTTPRequestHandler
 import selectors
-print("SEL:",hasattr(selectors, 'PollSelector'))
+if hasattr(selectors, 'PollSelector'):
+	_ServerSelector = selectors.PollSelector
+else:
+	_ServerSelector = selectors.SelectSelector
+print(_ServerSelector)
 
 
 ll = [0]
-h = [0]
+wp = [0]
 def log(*args, **kwargs):
 	with ll[0]:
 		print(*args, **kwargs)
@@ -39,8 +43,43 @@ def counter(_ll, name, event):
 def stopper(event):
 	event.wait()
 	log("GOT EVENT")
-	h[0]._BaseServer__shutdown_request = True
-	log("SET SHUTDOWN FLAG")
+	wp[0].kill()
+	log("KILLED WEB")
+
+
+def serve_forever(self, poll_interval=0.5):
+	"""Handle one request at a time until shutdown.
+
+	Polls for shutdown every poll_interval seconds. Ignores
+	self.timeout. If you need to do periodic tasks, do them in
+	another thread.
+	"""
+	self._BaseServer__is_shut_down.clear()
+	try:
+		# XXX: Consider using another file descriptor or connecting to the
+		# socket to wake this up instead of polling. Polling reduces our
+		# responsiveness to a shutdown request and wastes cpu at all other
+		# times.
+		log("1111111111111111")
+		with _ServerSelector() as selector:
+			selector.register(self, selectors.EVENT_READ)
+			log("22222222222222222")
+			while not self._BaseServer__shutdown_request:
+				log("3333333333333333")
+				ready = selector.select(poll_interval)
+				log("444444444444444")
+				# bpo-35017: shutdown() called during select(), exit immediately.
+				if self._BaseServer__shutdown_request:
+					break
+				if ready:
+					self._handle_request_noblock()
+
+				self.service_actions()
+			log("555555555555555")
+	finally:
+		self._BaseServer__shutdown_request = False
+		self._BaseServer__is_shut_down.set()
+	log("66666666666666")
 
 
 def main(_ll):
@@ -55,9 +94,9 @@ def main(_ll):
 	HOST = "0.0.0.0"
 	PORT = int(os.getenv("PORT", 80))
 	httpd = HTTPServer((HOST, PORT), SimpleHTTPRequestHandler)
-	h[0] = httpd
+	httpd.serve_forever = serve_forever
 	Thread(target=stopper, args=(event,)).start()
-	httpd.serve_forever()
+	httpd.serve_forever(httpd)
 	log("EXITING")
 	for p in processes:
 		p.kill()
@@ -68,6 +107,7 @@ if __name__ == '__main__':
 	multiprocessing.set_start_method("spawn")
 	ll[0]=Lock()
 	log(f"START METHOD = {multiprocessing.get_start_method()}")
-	Process(target=main, args=(ll[0],)).start()
+	wp[0]=Process(target=main, args=(ll[0],))
+	wp[0].start()
 	sleep(90)
 	log("ME")
