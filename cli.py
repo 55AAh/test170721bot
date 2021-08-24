17f147405b3201a24d13d8a417894ef1de4a8bca
55AAh <kostya.kulik@gmail.com>
2021-08-24 16:47:35 +0300

    Testing release phase time limit

--- cli.py
+++ cli.py
@@ -22,7 +22,7 @@ def do_request(host, command, retry=False, wait_ok=False):
 
 def execute(args):
     if args.command == "finish":
-        return do_request(args.host, "finish")
+        return do_request(args.host, "finish", wait_ok=True)
     if args.command == "shutdown":
         return do_request(args.host, "shutdown")
 
