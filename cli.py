888b53cb5c00f2d9e93e02cf60fdb4c81c27b856
55AAh <kostya.kulik@gmail.com>
2021-08-24 14:41:41 +0300

    Added shutdown to release phase

--- cli.py
+++ cli.py
@@ -1,4 +1,5 @@
 import argparse
+from urllib.parse import urljoin
 from time import sleep
 import requests
 
@@ -18,12 +19,12 @@ def do_request(address, timeout=None, reconnect=False):
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("command", choices=["shutdown"])
-    parser.add_argument("--host", default="http://127.0.0.1")
+    parser.add_argument("--host", default="https://127.0.0.1/")
     parser.add_argument("--timeout", type=int)
     parser.add_argument("--reconnect", action="store_const", const=True)
     args = parser.parse_args()
     if args.command == "shutdown":
-        print(do_request(args.host + "/api/shutdown", timeout=args.timeout, reconnect=args.reconnect))
+        print(do_request(urljoin(args.host, "/api/shutdown"), timeout=args.timeout, reconnect=args.reconnect))
 
 
 if __name__ == '__main__':
